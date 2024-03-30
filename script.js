cf8e09cfc06f3282de9585509578392f974d0f10
alejandromendoza1014 <amendoza208@student.cccd.edu>
2024-03-30 11:06:14 -0700

    commit

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
+
 function validateForm() {
   const form = document.forms['pet-info-form'];
   // TODO: Add checks here.
-  return true;
+  const bordetella = document.getElementById('bordetella');
+  const dhlpp = document.getElementById('dhlpp');
+  const rabies = document.getElementById('rabies');
+  let textbox = document.getElementById('other-info-text-area')
+
+  if (dhlpp.checked  && bordetella.checked && rabies.checked) {
+    return true;
+  } else {
+    textbox.focus();
+    textbox.setAttribute('placeholder', 'Please provide an explanation.')
+    if (textbox.value == '') {
+      return false;
+    }
+    else {
+    return true;
+    }
+  }
 }
